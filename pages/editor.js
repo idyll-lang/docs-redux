2b6c68c0f74588411480456cfe273b7d03b63827
Matthew Conlen <mc@mathisonian.com>
2018-02-04 22:36:53 -0800

    add editor route

---
+++ pages/editor.js
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+// import markdown from 'markdown-in-js'
+import Layout from '../components/layout'
+import Editor from '../components/editor'
+
+
+// const Content = () => markdown`
+// # GitHub
+// `
+
+
+export default ({ url }) => (
+  <Editor />
+)
